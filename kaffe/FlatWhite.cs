374d5be27419d7a028b75fbefafd58a40adcc6c8
Benjamin Kakar <benj3918@edu.easj.dk>
2018-09-05 14:04:59 +0200

    ændring

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        public FlatWhite()
+        {
+
+        }
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+        public string Styrke()
+        {
+            return "Mild";
+        }
+    }
+   
+}
